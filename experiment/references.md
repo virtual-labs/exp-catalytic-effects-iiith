aef021fbe3b2b4ee7f16b9332b8572ef7a1c5629
Jayant <54022294+PanwarJayant@users.noreply.github.com>
2021-08-05 15:40:08 +0530

    Testing: Code Review (#8)
    * added content
    
    * updated procedure
    
    * Update deployment-script.yml
    
    * added json script and assignment
    
    * fixed spacing issues
    
    * added simulation
    
    * Experiment update 1
    
    * References Correction
    
    * Assignment correction
    
    * Workflow update
    
    * Responsive Changes 1
    
    * Update procedure.md
    
    * Code Cleaning
    
    * Added questions
    
    Co-authored-by: balammaboya <balamma38@gmail.com>
    Co-authored-by: Pavan <24794698+pavanchow@users.noreply.github.com>
    Co-authored-by: sravanthimodepu <sravanthimodepu@gmail.com>
    Co-authored-by: sravanthimodepu <sravanthimodepu@users.noreply.github.com>

--- experiment/references.md
+++ experiment/references.md
@@ -1,1 +1,11 @@
-### Link your references in here
+**Book References:**
+
+1. T. K. Sau, A. Pal, and T. Pal, J. Phys. Chem. B 2001, 105, 9266-9272.
+2. T. K. Sau, A. L. Rogach, F. Jackel, T. A. Klar, and J. Feldmann, “Properties and Applications of Colloidal Nonspherical Noble Metal Nanoparticles”, Adv. Mater. 2010, 22, 1805–1825.
+3. Chemical Analysis: Modern Instrumentation Methods and Techniques, by Francis Rouessac and Annick Rouessac, Second Edition, John Wiley and Sons Ltd (2007).
+4. Across the Spectrum: Instrumentation for UV/Vis Spectrophotometry by Shane Beck, The Scientist 12[3]:20, Feb. 02, 1998.
+5. Physical Chemistry by Peter Atkins and Julio De Paula, Publisher: W. H. Freeman and Company (Dec 2009).
+6. Absorption and fluorescent properties of fluorescein by R. Sjoback, J. Nygren, and M. Kubista in journal Spectrochimica Acta Part A 1995, 51, L7 L21.
+7. Handbook of Instrumental Techniques for Analytical Chemistry, Frank A. Settla(Editor), Pearson Education Limited (2004).
+8. Modern Analytical Chemistry, David Harvey, McGraw-Hill Education (2000).
+9. Fundamentals of modern UV-visible spectroscopy, Primer, Tony Owen, Agilent Technologies (2000).
--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,469 @@
-//Your JavaScript goes in here
+"use strict";
+let overallIteration = -4;
+let divWidth;
+let videoSpeed = 1;
+let speedFactor = 1.0;
+let purple = "#886da7";
+let red = "#f54545";
+let yellow = "#ebe691";
+
+async function moveCuvette() {
+  let image = document.getElementById("cuvette");
+  image.setAttribute("opacity", "1");
+  let a1 = anime.timeline({
+    targets: "#cuvette",
+    duration: 800,
+    easing: "linear",
+  });
+  let transY = 300;
+  let transX = 200;
+  screenWidth();
+  if (divWidth < 769) {
+    transX = 100;
+    transY = 250;
+  }
+  if (overallIteration === 4) {
+    a1.add({
+      duration: 1000,
+      translateX: transX,
+      translateY: transY,
+      scale: 0.25,
+    })
+      .add({
+        opacity: 0,
+      })
+      .add({
+        translateX: 0,
+        translateY: 0,
+        scale: 1,
+      });
+
+    document.getElementById("instruction").innerHTML =
+      "Click on Observe button to observe what is happening inside the Spectrophotometer and choose video speed according to your own liking.";
+
+    document.getElementById("observation").innerHTML =
+      "Click on Observe button to observe what is happening inside the Spectrophotometer and choose video speed according to your own liking.";
+    overallIteration++;
+    restartAnimation = false;
+  }
+}
+
+let fillCuvette = async () => {
+  let path1 = document.getElementById("path1");
+  let path2 = document.getElementById("path2");
+  let finalPosition = 1;
+  let curPosition = 0;
+  while (true) {
+    if (curPosition > finalPosition) break;
+    curPosition += 0.01;
+    path1.setAttribute("offset", curPosition);
+    path2.setAttribute("offset", curPosition);
+    await new Promise((resolve) => setTimeout(resolve, 0.5));
+  }
+};
+
+let fillPipette = async (color) => {
+  const line = document.getElementById("half-grad2");
+  const yFinalPosition = 0;
+  document.getElementById("line").style.stopColor = color;
+  let yPos = 100;
+  const interval = window.setInterval(() => {
+    if (yPos < yFinalPosition) {
+      line.setAttribute("y1", "0.1%");
+      return window.clearInterval(interval);
+    }
+    yPos -= 0.6;
+    line.setAttribute("y1", `${yPos}%`);
+  }, 1);
+};
+
+async function pourGold() {
+  if (overallIteration === 1) {
+    changeMessage();
+
+    let image = document.getElementById("pipette");
+    image.setAttribute("opacity", "1");
+    image.style.pointerEvents = "none";
+    let a1 = anime.timeline({
+      targets: "#pipette",
+      duration: 800,
+      easing: "linear",
+    });
+    let startX = "-490%";
+    let startY = "-100%";
+
+    let endX = "2050%";
+    let endY = "-150%";
+
+    screenWidth();
+
+    if (divWidth < 769) {
+      startY = "-120%";
+      startX = "-480%";
+      endY = "700%";
+      endX = "0%";
+    }
+
+    a1.add({
+      duration: 0,
+      translateY: startY,
+      translateX: startX,
+    });
+    fillPipette(purple);
+    await new Promise((r) => setTimeout(r, 1000));
+    a1.add({
+      duration: 500,
+      translateX: endX,
+    })
+      .add({
+        duration: 900,
+        translateY: endY,
+        update: function (anim) {
+          fillCuvette();
+        },
+      })
+      .add({
+        opacity: 0,
+      });
+
+    document
+      .getElementById("eosin-beaker")
+      .setAttribute("onclick", "pourEosin()");
+    overallIteration++;
+    document.getElementById("gold-beaker").style.cursor = "default";
+    document.getElementById("eosin-beaker").style.cursor = "pointer";
+
+    if (restartAnimation) {
+      a1.restart();
+    }
+  }
+}
+
+async function pourEosin() {
+  if (overallIteration === 2) {
+    changeMessage();
+
+    let image = document.getElementById("pipette");
+    image.setAttribute("opacity", "1");
+    image.style.pointerEvents = "none";
+    let a1 = anime.timeline({
+      targets: "#pipette",
+      duration: 800,
+      easing: "linear",
+    });
+    let startX = "450%";
+    let startY = "-100%";
+
+    let endX = "2050%";
+    let endY = "-150%";
+
+    screenWidth();
+
+    if (divWidth < 769) {
+      startY = "-120%";
+      startX = "480%";
+      endY = "700%";
+      endX = "0%";
+    }
+
+    a1.add({
+      duration: 0,
+      translateY: startY,
+      translateX: startX,
+    });
+    fillPipette(red);
+    await new Promise((r) => setTimeout(r, 1000));
+    a1.add({
+      duration: 500,
+      translateX: endX,
+    })
+      .add({
+        duration: 800,
+        translateY: endY,
+      })
+      .add({
+        opacity: 0,
+      });
+
+    document
+      .getElementById("sodium-beaker")
+      .setAttribute("onclick", "pourSodium()");
+    overallIteration++;
+    document.getElementById("eosin-beaker").style.cursor = "default";
+    document.getElementById("sodium-beaker").style.cursor = "pointer";
+
+    if (restartAnimation) {
+      a1.restart();
+    }
+  }
+}
+
+async function pourSodium() {
+  if (overallIteration === 3) {
+    changeMessage();
+
+    let image = document.getElementById("pipette");
+    image.setAttribute("opacity", "1");
+    image.style.pointerEvents = "none";
+    let a1 = anime.timeline({
+      targets: "#pipette",
+      duration: 800,
+      easing: "linear",
+    });
+    let startX = "0%";
+    let startY = "180%";
+
+    let endX = "2050%";
+    let endY = "-150%";
+
+    screenWidth();
+
+    if (divWidth < 769) {
+      startY = "170%";
+      startX = "0%";
+      endY = "700%";
+      endX = "0%";
+    }
+
+    a1.add({
+      duration: 0,
+      translateY: startY,
+      translateX: startX,
+    });
+    fillPipette(yellow);
+    await new Promise((r) => setTimeout(r, 1000));
+    a1.add({
+      duration: 500,
+      translateX: endX,
+    })
+      .add({
+        duration: 800,
+        translateY: endY,
+      })
+      .add({
+        opacity: 0,
+      });
+
+    overallIteration++;
+    document.getElementById("sodium-beaker").style.cursor = "default";
+    document.getElementById("cuvette").style.cursor = "pointer";
+
+    if (restartAnimation) {
+      a1.restart();
+    }
+  }
+}
+
+let setupMessages = [
+  "Click on the Gold Seed Beaker option in the Apparatus Menu to introduce it into the workspace.",
+  "Click on the Eosin Beaker option in the Apparatus Menu to introduce it into the workspace.",
+  "Click on the Sodium Borohydride Beaker option in the Apparatus Menu to introduce it into the workspace.",
+  "Click on the Cuvette option in the Apparatus Menu to introduce it into the workspace.",
+  "Click on the Spectrophotometer option in the Apparatus Menu to introduce it into the workspace.",
+];
+
+let setup = 0;
+
+function setupMessage() {
+  document.getElementById("instruction").innerHTML = setupMessages[setup];
+  document.getElementById("observation").innerHTML = setupMessages[setup];
+  setup++;
+}
+
+setupMessage();
+async function visibility(x) {
+  if (x === 1 && overallIteration === -4) {
+    document.getElementById("gold-beaker").style.visibility = "visible";
+    overallIteration++;
+    setupMessage();
+  } else if (x === 2 && overallIteration === -3) {
+    document.getElementById("eosin-beaker").style.visibility = "visible";
+    overallIteration++;
+    setupMessage();
+  } else if (x === 3 && overallIteration === -2) {
+    document.getElementById("sodium-beaker").style.visibility = "visible";
+    overallIteration++;
+    setupMessage();
+  } else if (x === 4 && overallIteration === -1) {
+    document.getElementById("cuvette-row").style.visibility = "visible";
+    overallIteration++;
+    setupMessage();
+  } else if (x === 5 && overallIteration === 0) {
+    document.getElementById("spectro-row").style.visibility = "visible";
+    overallIteration++;
+    changeMessage();
+  }
+}
+
+let instructionMessages = [
+  "Click on the Gold Seed solution beaker to transfer small amount (around 2 mL) of the Gold Seed particle solution into the empty Cuvette",
+  "Click on the Eosin solution beaker to transfer 0.04 ml of Eosin solution to the Cuvette.",
+  "Click on the  Sodium Borohydride solution beaker to transfer 0.40 ml of Sodium Borohydride solution to the Cuvette.",
+  "Click on the Cuvette to place it into the Spectrophotometer.",
+];
+let iter1 = -1;
+function changeMessage() {
+  iter1++;
+  document.getElementById("instruction").innerHTML = instructionMessages[iter1];
+  document.getElementById("observation").innerHTML = instructionMessages[iter1];
+}
+
+document.getElementById("gold-beaker").style.cursor = "pointer";
+
+let iter2 = -1;
+let observationMessages = [
+  "Now observe the zoomed in animation of the Spectrophotometer. It can be seen that certain wavelengths of light are absorbed by our solution in the cuvette. The rest of the wavelengths are just transmitted back by the solution.",
+  "The points on the graph are used to depict the Wavelenghts of the lights absorbed.",
+];
+
+function observeMessage() {
+  if (restartAnimation) {
+    return;
+  }
+  iter2++;
+
+  document.getElementById("instruction").innerHTML = observationMessages[iter2];
+  document.getElementById("observation").innerHTML = observationMessages[iter2];
+}
+
+function screenWidth() {
+  divWidth = document.getElementById("workspace").clientWidth;
+}
+
+let originalSimulationHeight =
+  document.getElementById("simulation").clientHeight;
+
+document.getElementById("simulation").style.minHeight =
+  originalSimulationHeight + "px";
+
+let restartAnimation = false;
+
+async function restart() {
+  document.getElementById("simulation").style.height = originalSimulationHeight;
+
+  document.getElementById("animation-video").style.display = "none";
+  document.getElementById("plotted-graph-window").style.display = "none";
+
+  document.getElementById("head-instructions").innerHTML = "Instructions";
+  document.getElementById("head-observations").innerHTML = "Instructions";
+  document.getElementById("instruction").innerHTML = "";
+  document.getElementById("observation").innerHTML = "";
+  overallIteration = -4;
+  iter2 = -1;
+  iter1 = -1;
+  setup = 0;
+  setupMessage();
+  document.getElementById("syringe").style.opacity = 0;
+  document.getElementById("apparatus-bottles").style.display = "block";
+  document.getElementById("apparatus-spectro").style.display = "block";
+  document.getElementById("gold-beaker").style.visibility = "hidden";
+  document.getElementById("eosin-beaker").style.visibility = "hidden";
+  document.getElementById("sodium-beaker").style.visibility = "hidden";
+  document.getElementById("spectro-row").style.visibility = "hidden";
+  document.getElementById("cuvette-row").style.visibility = "hidden";
+  document.getElementById("slidecontainer").style.display = "none";
+  restartAnimation = true;
+
+  document.getElementById("eosin-beaker").style.cursor = "default";
+  document.getElementById("gold-beaker").style.cursor = "pointer";
+  document.getElementById("sodium-beaker").style.cursor = "default";
+
+  //Resetting the Cuvette
+  let path1 = document.getElementById("path1");
+  let path2 = document.getElementById("path2");
+  path2.setAttribute("offset", "0%");
+  path1.setAttribute("offset", "0%");
+  document.getElementById("cuvette").style.cursor = "default";
+  document.getElementById("cuvette").setAttribute("opacity", "1");
+}
+
+async function observe() {
+  if (overallIteration === 5) {
+    document.getElementById("slidecontainer").style.display = "block";
+    document.getElementById("apparatus-bottles").style.display = "none";
+    document.getElementById("apparatus-spectro").style.display = "none";
+    document.getElementById("animation-video").style.display = "block";
+    document.getElementById("animation-bottom-right").play();
+    document.getElementById("head-instructions").innerHTML = "Observations";
+    document.getElementById("head-observations").innerHTML = "Observations";
+    document.getElementById("observation").innerHTML = "";
+    document.getElementById("instruction").innerHTML = "";
+
+    observeMessage();
+
+    await new Promise((r) => setTimeout(r, 8000 * speedFactor));
+
+    if (!restartAnimation) {
+      overallIteration++;
+
+      document.getElementById("instruction").innerHTML =
+        "Click on Observe option in the Control Menu again to see the graph.";
+      document.getElementById("observation").innerHTML =
+        "Click on Observe option in the Control Menu again to see the graph.";
+    }
+  } else if (overallIteration === 6) {
+    observeMessage();
+
+    document.getElementById("slidecontainer").style.display = "none";
+
+    document.getElementById("animation-video").style.display = "none";
+    document.getElementById("plotted-graph-window").style.display = "block";
+    createGraph();
+
+    overallIteration++;
+    setTimeout(function () {
+      document.getElementById("instruction").innerHTML =
+        "Click on Restart option in the Control Menu to restart the experiment from scratch.";
+      document.getElementById("observation").innerHTML =
+        "Click on Restart option in the Control Menu to restart the experiment from scratch.";
+    }, 10000);
+  }
+}
+
+let gold = document.getElementById("gold-beaker");
+gold.addEventListener("click", pourGold);
+
+let cuvette = document.getElementById("cuvette");
+cuvette.addEventListener("click", function () {
+  moveCuvette(0);
+});
+
+let slider = document.getElementById("slider");
+let vid = document.getElementById("animation-bottom-right");
+slider.oninput = function () {
+  videoSpeed = slider.value;
+  vid.playbackRate = videoSpeed;
+  speedFactor = 1 / videoSpeed;
+};
+
+function createGraph() {
+  let trace1 = {
+    x: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
+    y: [
+      0.186, 0.2, 0.35, 0.4, 0.425, 0.438, 0.445, 0.45, 0.46, 0.468, 0.47,
+      0.471, 0.472, 0.473, 0.474, 0.475,
+    ],
+    mode: "lines",
+    line: {
+      color: "rgb(100, 0, 0)",
+      width: 3,
+    },
+  };
+
+  let data = [trace1];
+
+  var layout = {
+    title: "Absorbance over Time of Particle Solution",
+    xaxis: {
+      title: "Time (seconds)",
+      showgrid: true,
+      zeroline: true,
+    },
+    yaxis: {
+      title: "Absorbance at wavelenght 520nm",
+      showline: true,
+    },
+  };
+
+  Plotly.newPlot("chart-container", data, layout);
+}
